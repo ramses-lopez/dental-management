60236de94880125793ee1ddb2f34e643bb1da214
Ramsés López <ramses.lopezs@gmail.com>
2013-07-08 23:09:33 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DentoSpa::Application.config.secret_key_base = 'f2706e32e5db093f3d4b421d2982425f8d5951453e21176013e8ade13ff4901612e38edef26f20ce88e2b1b1f9dbcec1ebe6f53233178419814ba178cd40e00f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DentoSpa::Application.config.session_store :cookie_store, key: '_dento-spa_session'
