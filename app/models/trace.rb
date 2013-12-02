d4ba4b6dd1c6554ce00e2e2a189ac4e5d019f5fc
Ramsés López <ramses.lopezs@gmail.com>
2013-12-02 00:39:06 -0430

    mas cmabios para los reportes

--- app/models/trace.rb
+++ app/models/trace.rb
@@ -6,4 +6,15 @@ class Trace < ActiveRecord::Base
 
 	default_scope {order "created_at desc"}
 
+	scope :by_item_id, ->(item_id) {where(item_id: item_id)}
+	scope :by_user_id, ->(user_id) {where(user_id: user_id)}
+
+	def self.within(start_date, end_date)
+
+		start_date = DateTime.parse(start_date)
+		end_date = DateTime.parse(end_date).end_of_day
+
+    	where(created_at: start_date..end_date)
+  	end
+
 end
