7f5cb0c6f364e276b0e46619c82e37cd1a70f10c
Ramsés López <ramses.lopezs@gmail.com>
2013-12-28 15:45:35 -0430

    correccion de detalles en traces

--- app/controllers/traces_controller.rb
+++ app/controllers/traces_controller.rb
@@ -2,9 +2,9 @@ class TracesController < ApplicationController
 	def index
 
 		@traces = Trace.includes(:item, :user)
-		# @traces = @traces.by_user_id(params[:user_id]) unless params[:user_id].blank?
-		# @traces = @traces.by_item_id(params[:item_id]) unless params[:item_id].blank?
-		# @traces = Trace.within(params[:start_date], params[:end_date]).paginate(page: params[:page]) unless params[:start_date].blank? || params[:end_date].blank?
+		@traces = @traces.by_user_id(params[:user_id]) unless params[:user_id].blank?
+		@traces = @traces.by_item_id(params[:item_id]) unless params[:item_id].blank?
+		@traces = @traces.within(params[:start_date], params[:end_date]) unless params[:start_date].blank? || params[:end_date].blank?
 		@traces = @traces.paginate(page: params[:page])
 
 	end
