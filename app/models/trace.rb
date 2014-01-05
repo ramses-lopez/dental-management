61bab05e2c3f2722fa9bd383f441ee152bc91649
Ramsés López <ramses.lopezs@gmail.com>
2014-01-05 16:06:16 -0430

    correccion en el reporte de traces

--- app/models/trace.rb
+++ app/models/trace.rb
@@ -4,14 +4,14 @@ class Trace < ActiveRecord::Base
 	belongs_to :batch
 	belongs_to :user
 
-	default_scope {order "created_at desc"}
+	default_scope {order "traces.created_at desc"}
 
 	#TODO: Corregir consultas, adaptarlas para usar batches
-	scope :by_item_id, ->(item_id) {where(item_id: item_id)}
+	scope :by_item_id, ->(item_id) {joins(:batch).where("batches.item_id = ?", item_id)}
 	scope :by_user_id, ->(user_id) {where(user_id: user_id)}
 
 	def self.within(start_date, end_date)
-		start_date = DateTime.parse(start_date)
+		start_date = DateTime.parse(start_date).start_of_day
 		end_date = DateTime.parse(end_date).end_of_day
 
     	where(created_at: start_date..end_date)
